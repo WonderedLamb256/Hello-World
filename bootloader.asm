10dd910566aecadcd361b5bcbaf2b9e28e752faf
WonderedLamb256 <41864659+WonderedLamb256@users.noreply.github.com>
2020-01-09 17:46:20 -0500

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -1,2 +1,7 @@
-; 
-jmp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Programming Profile: x86 Assembly
+; Created: 1979 (Intel 8086)
+; Assembler used: NASM
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+BITS 16 ; NASM declaration to bit count
