cd587256bcc6d9e5ae2ee5b34bb5b62ddd79af70
WonderedLamb256 <41864659+WonderedLamb256@users.noreply.github.com>
2020-01-08 21:29:46 -0500

    Update codebreaker.c

--- codebreaker.c
+++ codebreaker.c
@@ -1,4 +1,27 @@
+// THE CHALLENGE - Find a rectangle with the same perimeter as area, while having its length be 3 inches more then its width.
 #include <stdio.h>
-int main {
 
+int main() {
+  int a; // x dimension 
+  int b; // y dimension
+  int c; // area
+  int d; // perimeter
+  int e; // equation
+  a = 1; // minimal possible
+  b = 4;
+  for(;c==d;) {
+    a++;
+    b++;
+    c = a*b;
+    d = a*2+b*2;
+    if(a+3 == b && c == d) {
+       e = c;
+       printf("Area/perimeter is %d", e, "\n");
+       e = a;
+       printf("Length is %d", e, "\n");
+       e = b;
+       printf("Width is %d", e, "\n");
+    }
+  }
+  return 0;
 }
